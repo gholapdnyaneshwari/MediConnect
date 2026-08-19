ad832672d695f3e333629371a9be257d5f1e229e
gholapdnyaneshwari <gholapdnyaneshwari31@gmail.com>
2026-08-19 21:18:20 +0530

    navbar hover

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -10,78 +10,167 @@ const Navbar = () => {
   return (
     <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400'>
 
+      {/* Logo */}
       <img
         onClick={() => navigate('/')}
         className='w-44 cursor-pointer'
         src={assets.logo}
-        alt="MediConnect"
+        alt='MediConnect'
       />
 
+      {/* Desktop Menu */}
       <ul className='hidden md:flex items-start gap-5 font-medium'>
 
-        <NavLink to="/" className="inline-block">
+        <NavLink to='/' className='inline-block'>
           <li className='py-1'>HOME</li>
           <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
         </NavLink>
 
-        <NavLink to="/doctors" className="inline-block">
+        <NavLink to='/doctors' className='inline-block'>
           <li className='py-1'>ALL DOCTORS</li>
           <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
         </NavLink>
 
-        <NavLink to="/about" className="inline-block">
+        <NavLink to='/about' className='inline-block'>
           <li className='py-1'>ABOUT</li>
           <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
         </NavLink>
 
-        <NavLink to="/contact" className="inline-block">
+        <NavLink to='/contact' className='inline-block'>
           <li className='py-1'>CONTACT</li>
           <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
         </NavLink>
 
       </ul>
 
+      {/* Right Side */}
       <div className='flex items-center gap-4'>
-        {
-          token
-            ? (
 
-              <div className='flex items-center gap-2 cursor-pointer group relative'>
-                <img className='w-8 rounded-full' src={assets.profile_pic} alt="" />
-                <img className='w-2.5' src={assets.dropdown_icon} alt="" />
-                <div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block'>
-                  <div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4'>
-                    <p onClick={() => navigate('my-profile')} className='hover:text-black cursor-pointer'>My Profile</p>
-                    <p onClick={() => navigate('my-appointments')} className='hover:text-black cursor-pointer'>My Appointments</p>
-                    <p onClick={() => setToken(false)} className='hover:text-black cursor-pointer'>Logout</p>
-                  </div>
-                </div>
+        {token ? (
+
+          /* Profile */
+          <div className='flex items-center gap-2 cursor-pointer group relative'>
+
+            <img
+              className='w-8 rounded-full'
+              src={assets.profile_pic}
+              alt=''
+            />
+
+            <img
+              className='w-2.5'
+              src={assets.dropdown_icon}
+              alt=''
+            />
+
+            {/* Profile Dropdown */}
+            <div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block'>
+              <div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4'>
+
+                <p
+                  onClick={() => navigate('/my-profile')}
+                  className='hover:text-black cursor-pointer'
+                >
+                  My Profile
+                </p>
+
+                <p
+                  onClick={() => navigate('/my-appointments')}
+                  className='hover:text-black cursor-pointer'
+                >
+                  My Appointments
+                </p>
+
+                <p
+                  onClick={() => setToken(false)}
+                  className='hover:text-black cursor-pointer'
+                >
+                  Logout
+                </p>
+
               </div>
+            </div>
 
-            )
-            : (
-              <button
-                onClick={() => navigate('/login')}
-                className='bg-primary text-white px-8 py-2 rounded-full font-light hidden md:block'
-              >
-                Create account
-              </button>
-            )
-        }
-        <img onClick={()=>setShowMenu(true)} className='w-6 md:hidden' src={assets.menu_icon} alt="" />
-        {/*-----------Moblie Menu-----------*/}
-        <div className={'md:hidden right-0 top-0 bottom-0 z-20 overflow-hidden hg-white transition'}>
-          <div>
-            <img src={assets.logo} alt=""/>
-            <img onClick={()=>setShowMenu(false)} src={assets.cross_icon}  alt=''/>
           </div>
-          <ul>
-            <NavLink>Home</NavLink>
-            <NavLink>ALL DOCTORS</NavLink>
-            <NavLink>ABOUT</NavLink>
-            <NavLink>CONTACT</NavLink>
+
+        ) : (
+
+          /* Create Account */
+          <button
+            onClick={() => navigate('/login')}
+            className='bg-primary text-white px-8 py-2 rounded-full font-light hidden md:block'
+          >
+            Create account
+          </button>
+
+        )}
+
+        {/* Mobile Menu Icon */}
+        <img
+          onClick={() => setShowMenu(true)}
+          className='w-6 md:hidden cursor-pointer'
+          src={assets.menu_icon}
+          alt=''
+        />
+
+        {/* Mobile Menu */}
+        <div
+          className={`${
+            showMenu ? 'fixed w-full' : 'h-0 w-0'
+          } md:hidden right-0 top-0 bottom-0 z-20 overflow-hidden bg-white transition-all`}
+        >
+
+          {/* Mobile Menu Header */}
+          <div className='flex items-center justify-between px-5 py-6'>
+            <img
+              className='w-36'
+              src={assets.logo}
+              alt='MediConnect'
+            />
+
+            <img
+              onClick={() => setShowMenu(false)}
+              className='w-7 cursor-pointer'
+              src={assets.cross_icon}
+              alt=''
+            />
+          </div>
+
+          {/* Mobile Links */}
+          <ul className='flex flex-col gap-2 px-5 mt-5 text-lg font-medium'>
+
+            <NavLink 
+              onClick={() => setShowMenu(false)}
+              to='/'
+            >
+              <p className='px-4 py-2 rounded inline-block'>HOME</p>
+            </NavLink>
+
+            <NavLink 
+              onClick={() => setShowMenu(false)}
+              to='/doctors'
+            >
+             <p className='px-4 py-2 rounded inline-block'>ALL DOCTORS</p> 
+            </NavLink>
+
+            <NavLink 
+              onClick={() => setShowMenu(false)}
+              to='/about'
+            >
+              <p className='px-4 py-2 rounded inline-block'>ABOUT</p>
+            </NavLink>
+
+            <NavLink 
+              onClick={() => setShowMenu(false)}
+              to='/contact'
+            >
+              <p className='px-4 py-2 rounded inline-block'>CONTACT</p>
+            </NavLink>
+
           </ul>
+
         </div>
+
       </div>
 
     </div>
