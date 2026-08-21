943712c777aa3abf2e8855dbee7cfe5c966cb4d5
gholapdnyaneshwari <gholapdnyaneshwari31@gmail.com>
2026-08-21 19:03:08 +0530

    backend structure create

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import 'dotenv/config';
+import connectDB from "./config/mongodb.js";
+import connectCloudinary from "./config/cloudinary.js";
+
+const app = express()
+const port = process.env.PORT || 4000
+connectDB()
+connectCloudinary()
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API WORKING");
+});
+
+app.listen(port, () => console.log("Server Started",port))
